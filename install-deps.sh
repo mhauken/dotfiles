9c716fe8ad469c5bff25ea38c408e9aa8e14fe9e
Marius Hauken <hauken@netliferesearch.com>
2014-02-15 16:07:29 +0100

    my dotfiles

---
+++ install-deps.sh
@@ -0,0 +1,36 @@
+# Copied from paul irish. Different useful commands to run to set up your computer
+
+
+# https://github.com/rupa/z
+# z, oh how i love you
+cd ~/code
+git clone https://github.com/rupa/z.git
+chmod +x ~/code/z/z.sh
+# also consider moving over your current .z file if possible. it's painful to rebuild :)
+
+# z binary is already referenced from .bash_profile
+
+
+# https://github.com/dronir/SpotifyControl
+# Spotify Controll Script
+cd ~/code
+git clone git://github.com/dronir/SpotifyControl.git
+
+# https://github.com/jeroenbegyn/VLCControl
+# VLC Controll Script
+cd ~/code
+git clone git://github.com/jeroenbegyn/VLCControl.git
+
+
+# my magic photobooth symlink -> dropbox. I love it.
+# first move Photo Booth folder out of Pictures
+# then start Photo Booth. It'll ask where to put the library.
+# put it in Dropbox/public
+
+# now you can record photobooth videos quickly and they upload to dropbox DURING RECORDING
+# then you grab public URL and send off your video message in a heartbeat.
+
+
+# chrome canary as default
+# on a mac you can set chrome canary as your default inside of Safari preferences :)
+
