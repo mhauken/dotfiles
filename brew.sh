852838626fcccf8a442682808defaf61dc705a64
Marius Hauken <marius.hauken@gmail.com>
2015-09-15 15:51:08 +0200

    Added craft-instructions

--- brew.sh
+++ brew.sh
@@ -12,6 +12,13 @@ brew install git
 brew install node # This installs `npm` too using the recommended installation method
 brew git-ftp
 
+# For a lot of developement:
+brew tap homebrew/dupes
+brew tap homebrew/versions
+brew tap homebrew/homebrew-php
+brew install php56 php56-mcrypt
+brew link php56
+
 
 # Remove outdated versions from the cellar
 brew cleanup
