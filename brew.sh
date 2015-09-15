b3f9f90bc3220b1f1e961ff334a66ccab5cf97f3
Marius Hauken <marius.hauken@gmail.com>
2015-09-15 15:36:04 +0200

    added brew.sh

---
+++ brew.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Install command-line tools using Homebrew
+
+# Make sure we’re using the latest Homebrew
+brew update
+
+# Upgrade any already-installed formulae
+brew upgrade
+
+brew install git
+brew install node # This installs `npm` too using the recommended installation method
+brew install caskroom/cask/brew-cask
+brew git-ftp
+
+
+# Remove outdated versions from the cellar
+brew cleanup
