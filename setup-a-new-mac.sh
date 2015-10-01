165f706e2ab24dd2df68cc58c39f0272cd1d09a4
Marius Hauken <marius.hauken@gmail.com>
2015-10-01 22:49:43 +0200

    Update setup-a-new-mac.sh

--- setup-a-new-mac.sh
+++ setup-a-new-mac.sh
@@ -33,7 +33,7 @@ curl https://install.meteor.com | /bin/sh
 npm install -g git-open
 
 # github.com/rupa/z   - oh how i love you
-git clone https://github.com/rupa/z.git ~/code/z
-chmod +x ~/code/z/z.sh
+git clone https://github.com/rupa/z.git ~/z
+chmod +x ~/z/z.sh
 # consider reusing your current .z file if possible. it's painful to rebuild :)
 # z is hooked up in .bash_profile
