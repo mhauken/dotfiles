3409d09d24200275d548526d6b90a2e34c4178f5
Marius Hauken <marius.hauken@gmail.com>
2017-01-05 10:06:07 +0100

    added nvm to node-file

--- node.sh
+++ node.sh
@@ -1,2 +1,6 @@
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash #installs nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+nvm install node # downloads latest node versjon
 npm install -g bower
 npm install -g gulp
