7be9c4331bdc2312681a5cf6f9c03802ddb53060
Marius Hauken <marius.hauken@gmail.com>
2018-02-05 10:46:02 +0100

    removed php-specific stuff

--- brew.sh
+++ brew.sh
@@ -16,20 +16,10 @@ brew upgrade
 
 brew install git
 brew install git-open ## Open GitHub webpages from a terminal by writing `git-open`
-brew install git-ftp
 brew install mas # This installs Mac App Store command line interface
 brew install heroku
 brew install heroku-toolbelt
 brew install yarn
-
-# For a lot of developement:
-brew tap homebrew/dupes
-brew tap homebrew/versions
-brew tap homebrew/homebrew-php
-brew install php70 php70-mcrypt
-brew link php70
-brew install rbenv ruby-build
-
 
 # Remove outdated versions from the cellar
 brew cleanup
