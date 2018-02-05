07bbbd60bb667254df16167d05542512ac6e7f8b
Marius Hauken <marius.hauken@gmail.com>
2018-02-05 10:58:26 +0100

    removed bower and gulp. Hardly use these anymore

--- node.sh
+++ node.sh
@@ -2,5 +2,3 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | b
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
 nvm install node # downloads latest node versjon
-npm install -g bower
-npm install -g gulp
