9c06ba38b58715d58e84f6e1565a436c6323126f
Marius Hauken <marius.hauken@gmail.com>
2018-04-20 11:02:52 +0200

    new nvm-version

--- node.sh
+++ node.sh
@@ -1,4 +1,4 @@
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash #installs nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.9/install.sh | bash #installs nvm
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
 nvm install node # downloads latest node versjon
