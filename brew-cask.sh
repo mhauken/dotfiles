aeb6b870db5488f11e34ea6192f8439dbf189b37
Marius Hauken <marius.hauken@gmail.com>
2021-02-26 09:43:18 +0100

    update apps

--- brew-cask.sh
+++ brew-cask.sh
@@ -13,19 +13,21 @@ brew tap caskroom/versions
 # daily
 brew cask install figma
 brew cask install cleanshot
+brew cask install obsidian
 
 # dev
 brew cask install hyper
 brew cask install visual-studio-code
 brew cask install tower
+brew cask install webstorm
 
 # browsers
 brew cask install brave
 brew cask install firefoxnightly
 
 # less often
-brew cask install disk-inventory-x
 brew cask install spotify
+brew cask install sonos
 brew cask install tripmode
 
 
@@ -34,9 +36,6 @@ brew cask install tripmode
 echo What is your Mac app-store username e-mail?
 read username
 mas signin $username
-mas install 409183694 # Keynote
-mas install 408981434 # iMovie
-mas install 443987910 # 1Password
 mas install 413965349 # Soulver
 mas install 904280696 # Things3
 mas install 1254981365 # Contrast
@@ -45,5 +44,4 @@ mas install 972028355 # Micro Snitch
 mas install 975937182 # Fantastical 2
 mas install 1016366447 # Bear
 mas install 937984704 # Amphetamine
-mas install 507874739 # Google Drive
 mas install 985367838 # Outlook
