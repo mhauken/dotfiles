60df157a357d81856f3942c555f62228f2790328
Marius Hauken <marius.hauken@gmail.com>
2024-10-06 07:05:26 +0200

    Update node.sh

--- node.sh
+++ node.sh
@@ -1,4 +1,4 @@
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
 nvm install node # downloads latest node versjon
